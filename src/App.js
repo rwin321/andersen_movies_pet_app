d82cd21c2c8ae6041163edb387b56d280972e4ea
apollon313131 <apollo-bel@mail.ru>
2021-05-18 20:30:17 +0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import "./styles/main.scss";
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import ContentView from "./components/ContentView";
+import './styles/ContentView.css';
 
 const App = () => {
-  const [state, setstate] = useState('');
-  const { homepage, id, imdb_id, original_language, poster_path } = state
-
+  const [content, setContent] = useState([]);
+  // const { homepage, id, imdb_id, original_language, poster_path } = state
+  // https://api.themoviedb.org/3/trending/all/week?api_key=cd524ad267ee1be15d8602d5d5bdecad
+  // https://api.themoviedb.org/3/movie/550?api_key=cd524ad267ee1be15d8602d5d5bdecad?
   const fetchRequest = async () => {
-    let response = await fetch('https://api.themoviedb.org/3/movie/550?api_key=cd524ad267ee1be15d8602d5d5bdecad');
-    let result = await response.json()
-    setstate(result)
+    const { data } = await axios.get('https://api.themoviedb.org/3/trending/all/week?api_key=cd524ad267ee1be15d8602d5d5bdecad')
+    // let response = await fetch('https://api.themoviedb.org/3/trending/all/week?api_key=cd524ad267ee1be15d8602d5d5bdecad');
+    // let result = await response.json()
+    // console.log(result);
+    setContent(data.results)
   }
 
   useEffect(() => {
     fetchRequest()
   }, [])
 
+  
   return (
     <div className="App">
-      <div>APP</div>
-      <div>
-        <img
-          src={`https://image.tmdb.org/t/p/original/${poster_path}`}
-          alt="MovieCover"
-        />
-        {/* <p>{state.homepage}</p>
-        <p>{state.id}</p> */}
-        <p>{homepage}</p>
-        <p>{id}</p>
+      <div className='trending'>
+        {
+          content && content.map((c)=> (
+            <ContentView
+            key={c.id}
+            id={c.id}
+            poster={c.poster_path}
+            title={c.title || c.name}
+            media_type={c.media_type}
+            date={c.first_air_date || c.release_date}
+            />
+          ))
+        }
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default App;
 
---
+++ src/components/ContentView.js
@@ -0,0 +1,22 @@
+import '../styles/ContentView.css';
+
+const ContentView = ({
+    id,
+    poster,
+    title,
+    date,
+    media_type,
+}) => {
+    return <div className='media'>
+       <img 
+            className='poster'
+          src={`https://image.tmdb.org/t/p/w300/${poster}`}
+          alt="MovieCover"
+        />
+        <b className='title'>{title}</b>
+        <span>{media_type === 'tv'?'TV Series':'Movie'}</span>
+        <span className='DownTitle'>{date}</span>
+    </div>
+}
+
+export default ContentView
